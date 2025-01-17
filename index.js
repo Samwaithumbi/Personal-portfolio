c5e13580d2c148dbc9f5bdf91ee7af30f229f870
Samwaithumbi <sammwangi7965@gmail.com>
2025-01-17 17:48:06 +0300

    Change

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const toggleBtn = document.getElementById('toggle-btn');
+const navBar = document.getElementById('nav-bar');
+
+toggleBtn.addEventListener('click', () => {
+    if (navBar.style.right === '-300px') {
+        navBar.style.right = '0';
+    } else {
+        navBar.style.right = '-300px';
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (window.innerWidth <= 500 && !navBar.contains(event.target) && !toggleBtn.contains(event.target)) {
+        navBar.style.right = '-300px';
+    }
+});
+
+const navLinks = document.querySelectorAll('#nav-bar a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (window.innerWidth <= 500) {
+            navBar.style.right = '-300px';
+        }
+    });
+});
 const seeMore=document.getElementById("see-more");
 const about=document.getElementById("about");
-
-seeMore.addEventListener("click", ()=>{
-
-})
 
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbzxaCQeBDk86oX_7Z0ZinoFq8oVArjkIQ8NoJKqRfkUoxUFgRbxYwrGyV9qSdemgmjBzw/exec'
