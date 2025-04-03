597cc4304659fa31d66568f4ecc876f33a90c627
Samwaithumbi <sammwangi7965@gmail.com>
2025-04-03 18:31:15 +0300

    form submission

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ navLinks.forEach(link => {
         }
     });
 });
-const seeMore=document.getElementById("see-more");
-const about=document.getElementById("about");
 
 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzxaCQeBDk86oX_7Z0ZinoFq8oVArjkIQ8NoJKqRfkUoxUFgRbxYwrGyV9qSdemgmjBzw/exec'
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzP2RAvq-gwKRa9a00utpkDGZC1MnzL11jWegKIghawzEKd99PwnCBQ89xRbeI0mKeJ/exec'
   const form = document.forms['submit-to-google-sheet']
   const msg=document.getElementById("msg")
 
@@ -40,7 +38,7 @@ const about=document.getElementById("about");
         msg.innerHTML="Message sent successfully"
         setTimeout(function(){
             msg.innerHTML=""
-        },5000)
+        },3000)
         form.reset()
       })
       .catch(error => console.error('Error!', error.message))
